eba4bb1f688faef3229ec3be786969e6b13a8e9b
CJduncan <136113552+CJduncan@users.noreply.github.com>
2023-11-04 22:19:46 -0600

    working on customizer
    when button clicekd lead to customizer page, and back button added as well

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Canvas from './Canvas/index';
 import Home from "./pages/Home";
-import Custom from './pages/Custom';
+import Customizer from './pages/Customizer';
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <main className="app transition-all ease-in">
       <Home />
       <Canvas />
-      <Custom />
+      <Customizer />
     </main>
   );
 }
