609f5de31a6844ef7ed3c1ca5dbdb26414d2e62b
CJduncan <136113552+CJduncan@users.noreply.github.com>
2023-11-06 13:29:51 -0700

    fixed bug
    something wrogn with the shirt.jsx, wouldnt render the file

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -14,47 +14,42 @@ const Shirt = () => {
 
     fullTexture.anisotropy = 16;
 
-    useFrame((state,delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
-    const statesString = JSON.stringify(snap);
+    useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
 
-  return (
-    <group key={statesString}>
-        <mesh 
-         castShadow geometry={nodes.T_Shirt_male.geometry}
-         material={materials.lambert1}
-         material-roughness={1}
-         dispose={null}
-        
+    const stateString = JSON.stringify(snap);
+  
+    return (
+      <group key={stateString}>
+        <mesh
+          castShadow
+          geometry={nodes.T_Shirt_male.geometry}
+          material={materials.lambert1}
+          material-roughness={1}
+          dispose={null}
         >
-            {snap.isFullTexture && (
-                <Decal 
-                position={[0,0,0]}
-                rotation={[0,0,0]}
-                scale={[1]}
-                map={fullTexture}
-                
-                />
-
-            )}
-            {snap.isLogoTexture && (
-                <Decal 
-                position={[0,0,0]}
-                rotation={[0,0,0]}
-                scale={[1]}
-                map={fullTexture}
-                depthTest={false}
-                depthWrite={true}
-                
-                
-                />
-
-            )}
-        
-           
-        
+          {snap.isFullTexture && (
+            <Decal 
+              position={[0, 0, 0]}
+              rotation={[0, 0, 0]}
+              scale={1}
+              map={fullTexture}
+            />
+          )}
+  
+          {snap.isLogoTexture && (
+            <Decal 
+              position={[0, 0.04, 0.15]}
+              rotation={[0, 0, 0]}
+              scale={0.15}
+              map={logoTexture}
+              
+              depthTest={false}
+              depthWrite={true}
+            />
+          )}
         </mesh>
-    </group>
-  )
-}
-
-export default Shirt
+      </group>
+    )
+  }
+  
+  export default Shirt
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -19,9 +19,9 @@ const Customizer = () => {
   const [file, setFile] = useState('');
 
   const [prompt, setPrompt] = useState('');
-  const[generatingImg, setGeneratingImg] = useState(false);
+  const [generatingImg, setGeneratingImg] = useState(false);
 
-  const [activeEditorTab, setActiveEditorTab] = useState('');
+  const [activeEditorTab, setActiveEditorTab] = useState("");
   const [activeFilterTab, setActiveFilterTab] = useState({
     logoShirt: true,
     stylishShirt: false,
@@ -30,25 +30,24 @@ const Customizer = () => {
 
   const generateTabContent = () => {
     switch (activeEditorTab) {
-      case 'colorpicker':
+      case "colorpicker":
         return <ColorPicker />
-      case 'filepicker':
-        return <FilePicker 
-        file={file}
-        setFile={setFile}
-        readFile={readFile}
-
+      case "filepicker":
+        return <FilePicker
+          file={file}
+          setFile={setFile}
+          readFile={readFile}
         />
-      case 'aipicker':
-        return <AiPicker />
-        
-        
-    
+      case "aipicker":
+        return <AIPicker 
+          prompt={prompt}
+          setPrompt={setPrompt}
+          generatingImg={generatingImg}
+          handleSubmit={handleSubmit}
+        />
       default:
         return null;
     }
-    
-
   }
   const handleDecals = (type, result) => {
     const decalType = DecalTypes[type];
@@ -91,53 +90,59 @@ const Customizer = () => {
         setActiveEditorTab("");
       })
   }
-
   return (
     <AnimatePresence>
-      {!snap.intro && (
-        <>
-          <motion.div key ="custom" className='absolute top-0 left-0 z-10' {...slideAnimation('left')}>
-            <div className='flex items-center min-h-screen'>
-              <div className='editortabs-container tabs'>
-                {EditorTabs.map((tab) => (
-                  <Tab
+    {!snap.intro && (
+      <>
+        <motion.div
+          key="custom"
+          className="absolute top-0 left-0 z-10"
+          {...slideAnimation('left')}
+        >
+          <div className="flex items-center min-h-screen">
+            <div className="editortabs-container tabs">
+              {EditorTabs.map((tab) => (
+                <Tab 
                   key={tab.name}
                   tab={tab}
                   handleClick={() => setActiveEditorTab(tab.name)}
-                  />
+                />
+              ))}
 
-                ))}
-                {generateTabContent()}
-
-              </div>
+              {generateTabContent()}
             </div>
-          </motion.div>
-          <motion.div className='absolute z-10 top-5 right-5' {...fadeAnimation}>
-            <CustomButton 
-              type='filled'
-              title='Go Back'
-              handleClick={() =>  state.intro =true}
-              customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+          </div>
+        </motion.div>
+
+        <motion.div
+          className="absolute z-10 top-5 right-5"
+          {...fadeAnimation}
+        >
+          <CustomButton 
+            type="filled"
+            title="Go Back"
+            handleClick={() => state.intro = true}
+            customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+          />
+        </motion.div>
+
+        <motion.div
+          className='filtertabs-container'
+          {...slideAnimation("up")}
+        >
+          {FilterTabs.map((tab) => (
+            <Tab
+              key={tab.name}
+              tab={tab}
+              isFilterTab
+              isActiveTab={activeFilterTab[tab.name]}
+              handleClick={() => handleActiveFilterTab(tab.name)}
             />
-
-          </motion.div>
-
-          <motion.div className='filtertabs-container ' {...slideAnimation('up')}>
-            {FilterTabs.map((tab) => (
-                    <Tab
-                    key={tab.name}
-                    tab={tab}
-                    isFilterTab
-                    isActiveTab=''
-                    handleClick={() => {}}
-                    />
-
-                  ))}
-
-          </motion.div>
-        </>
-      )}
-    </AnimatePresence>
+          ))}
+        </motion.div>
+      </>
+    )}
+  </AnimatePresence>
   )
 }
 
